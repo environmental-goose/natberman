cd5cc25b1252d31d2eb6a45f975a9fcd970eaf20
environmental-goose <natberman01@gmail.com>
2026-01-20 14:48:40 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
-import { componentTagger } from "lovable-tagger";
 import path from "path";
 
 export default defineConfig(({ mode }) => ({
-  // ADD THIS LINE BELOW
-  base: "/", 
+  // Set base to your repository name
+  base: "/natberman/",
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
